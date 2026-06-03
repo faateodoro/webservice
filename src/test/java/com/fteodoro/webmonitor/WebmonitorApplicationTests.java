bcae99997db1d42a7e906f8f3880239c3479c32e
Fabiano Teodoro <faateodoro@gmail.com>
2026-06-03 00:08:07 -0300

    Inital commit

---
+++ src/test/java/com/fteodoro/webmonitor/WebmonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fteodoro.webmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebmonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
