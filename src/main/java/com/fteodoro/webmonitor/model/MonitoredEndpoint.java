7de21964c0b531bf070198c228fa102dba55d2b8
Fabiano Teodoro <faateodoro@gmail.com>
2026-06-03 00:09:47 -0300

    Add MonitoredEndpoint class

---
+++ src/main/java/com/fteodoro/webmonitor/model/MonitoredEndpoint.java
@@ -0,0 +1,62 @@
+package com.fteodoro.webmonitor.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class MonitoredEndpoint {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String url;
+
+    @Column(nullable = false)
+    private int checkIntervalSeconds;
+
+    @Column(nullable = false)
+    private boolean isActive;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    public MonitoredEndpoint(String name, String url, int checkIntervalSeconds) {
+        this.name = name;
+        this.url = url;
+        this.checkIntervalSeconds = checkIntervalSeconds;
+        this.isActive = true;
+    }
+
+    protected MonitoredEndpoint() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public int getCheckIntervalSeconds() {
+        return checkIntervalSeconds;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+}
