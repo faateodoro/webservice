b75549499512d0e07dc2f705bcc2d2659d9ae3a5
Fabiano Teodoro <faateodoro@gmail.com>
2026-07-01 00:22:37 -0300

    Creating the scheduler

--- src/main/java/com/fteodoro/webmonitor/WebmonitorApplication.java
+++ src/main/java/com/fteodoro/webmonitor/WebmonitorApplication.java
@@ -2,12 +2,13 @@ package com.fteodoro.webmonitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class WebmonitorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebmonitorApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(WebmonitorApplication.class, args);
+    }
 }
---
+++ src/main/java/com/fteodoro/webmonitor/scheduler/MonitoringScheduler.java
@@ -0,0 +1,29 @@
+package com.fteodoro.webmonitor.scheduler;
+
+import com.fteodoro.webmonitor.model.MonitoredEndpoint;
+import com.fteodoro.webmonitor.service.EndpointCheckerService;
+import com.fteodoro.webmonitor.service.MonitoredEndpointService;
+import java.util.List;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MonitoringScheduler {
+
+    private final EndpointCheckerService endpointCheckerService;
+    private final MonitoredEndpointService monitoredEndpointService;
+
+    public MonitoringScheduler(
+        MonitoredEndpointService monitoredEndpointService,
+        EndpointCheckerService endpointCheckerService
+    ) {
+        this.endpointCheckerService = endpointCheckerService;
+        this.monitoredEndpointService = monitoredEndpointService;
+    }
+
+    @Scheduled(fixedDelay = 30000)
+    public void scheduleChecking() {
+        List<MonitoredEndpoint> endpoints = monitoredEndpointService.findAll();
+        endpoints.forEach(endpointCheckerService::check);
+    }
+}
